6dcd2839c353f8e0209046b7cf92f7ed23ec9b45
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2016-10-11 22:49:01 +0200

    Added learning code in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 //TextGun model
 #include "TextGun.hpp"
 
+#include <string>//Strings
+
+#include <fstream>//File stream
+
+#include <sstream>//String stream
+
+//File to open
+const std::string FILE_NAME("TextLearning.txt");
+
 int main()
 {
+    TextGun::WordModel model;//Model to be trained
+    std::ifstream txt_stream(FILE_NAME);//Open the file with the text
+    std::string buffer;//Hold the last read line
+    while(std::getline(txt_stream,buffer))//Load a line from the file
+    {
+        std::stringstream ss(buffer);//Create a string stream
+        TextGun::TextStream ts(ss);//Create a text stream
+        model.learn(ts);//Learn from this line
+    }
+
     return 0;
 }
